0bf509a7578dba17419a1ba009a3a32efddedcef
simyuseon <yseon0508@naver.com>
2023-05-04 12:42:51 +0900

    Feat: 이미지 최적화 학습

--- app/list/page.js
+++ app/list/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image"; //이미지 최적화
+
 export default function List() {
   let 상품 = ["Tomatos", "Pasta", "Coconut"];
 
@@ -7,6 +9,15 @@ export default function List() {
       {상품.map((a, i) => {
         return (
           <div className="food" key={i}>
+            <Image
+              src={`/food${i}.png`}
+              alt="토마토"
+              className="food-img"
+              width={400}
+              height={300}
+            />
+            {/* 외부이미지는 width, heigth속성 필요 */}
+            {/* next.config.js 셋팅도 필요 */}
             <h4>{a} $40</h4>
           </div>
         );
