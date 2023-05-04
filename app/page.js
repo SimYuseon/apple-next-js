ffc9ea5afec460aae481495dde48b20f910b0d7c
simyuseon <yseon0508@naver.com>
2023-05-04 12:12:35 +0900

    Feat: link,rounting,layout.js 학습

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 export default function Home() {
   let name = "seon";
-  let link = "http";
+
   return (
     <div>
-      <h4 style={{ color: "red", fontSize: "30px" }}>애플후레시</h4>
+      <h4 className="title">애플후레시</h4>
       <p className="title-sub">by dev {name}</p>
-      <a href={link}>링크</a>
     </div>
   );
 }
